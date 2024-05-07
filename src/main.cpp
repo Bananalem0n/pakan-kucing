420ca19066e9105da70125496119a0b8cd62bfd1
Ukasyah Nata <ukasyahnata@outlook.com>
2024-05-07 16:24:19 +0700

    update to sync data when connected

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,7 @@
 #define BLYNK_PRINT Serial
-#define BLYNK_TEMPLATE_ID "TMPL670vbMUX0"
-#define BLYNK_TEMPLATE_NAME "pakan kucing"
-#define BLYNK_AUTH_TOKEN ""
+#define BLYNK_TEMPLATE_ID "TMPL6WTerXsMv"
+#define BLYNK_TEMPLATE_NAME "Kucing"
+#define BLYNK_AUTH_TOKEN "x157tfHtNqjX-PCk-aITZ22cgOnyLUYC"
 
 #include <ESP8266WiFi.h>
 #include <BlynkSimpleEsp8266.h>
@@ -11,10 +11,17 @@ char auth[] = BLYNK_AUTH_TOKEN; // token dapat dicek di email
 char ssid[] = "..";             // nama wifi
 char pass[] = "12345678";       // password wifi
 
-Servo servo;
-int pinServo = 2;
+Servo servo; // initiate servo class library
+int pinServo = 2; // servo pin on microcontroller
+int maxAngle = 180; // angle servo max
+
 int pos = 0;
-int maxAngle = 90;
+
+BlynkTimer timer;
+
+BLYNK_CONNECTED() {
+   Blynk.syncVirtual(V1);
+}
 
 BLYNK_WRITE(V1)
 {
@@ -42,13 +49,20 @@ BLYNK_WRITE(V1)
   }
 }
 
+void sendServoAngle()
+{
+  Blynk.virtualWrite(V2, pos); // Send servo angle to virtual pin 2
+}
+
 void setup()
 {
   Serial.begin(115200);
   Blynk.begin(auth, ssid, pass);
+  timer.setInterval(5000L, sendServoAngle); // Set timer to trigger every 5 seconds
 }
 
 void loop()
 {
   Blynk.run();
+  timer.run(); // Run Blynk timer
 }
